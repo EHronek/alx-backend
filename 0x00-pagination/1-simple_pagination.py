15ba2aff93251c8a400bf925ba1d649343523a43
EHronek <emmanuelhronek@gmail.com>
2025-05-12 23:11:50 +0300

    simple pagination fix

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -35,7 +35,7 @@ class Server:
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Function should retrieve a page of data.
         """
-        assert type(page) == int and type(page_size) == int
+        assert type(page) is int and type(page_size) is int
         assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
         data = self.dataset()
