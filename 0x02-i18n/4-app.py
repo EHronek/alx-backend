d8e51af3c01104ae658186c2781c59fed3588a49
EHronek <emmanuelhronek@gmail.com>
2025-05-23 22:23:28 +0300

    parametrize templates

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -23,15 +23,10 @@ babel = Babel(app)
 def get_locale() -> str:
     """Gets the locale for a web page.
     """
-    queries = request.query_string.decode('utf-8').split('&')
-    query_table = dict(map(
-        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
-        queries,
-    ))
-    if 'locale' in query_table:
-        if query_table['locale'] in app.config["LANGUAGES"]:
-            return query_table['locale']
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
